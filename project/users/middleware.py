09c141706f55240e580966b10341efb774b11a7c
sunil1206 <masunil12@gmail.com>
2025-11-01 10:50:37 +0100

    update dash board

--- project/users/middleware.py
+++ project/users/middleware.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib import messages
-from .models import Subscription # Import your model
+from .models import Subscription
 
 class SubscriptionCheckMiddleware:
     def __init__(self, get_response):
@@ -13,7 +13,7 @@ class SubscriptionCheckMiddleware:
 
             # Define pages that an expired user can still access
             safe_paths = [
-                reverse('subscription_renew'),  # The page you ask them to pay on
+                reverse('users:subscription_renew'),  # <-- THE FIX
                 reverse('account_logout'),      # The logout URL
                 # Add any other "safe" URLs, like a contact page
             ]
@@ -26,12 +26,12 @@ class SubscriptionCheckMiddleware:
             try:
                 if not request.user.subscription.is_valid:
                     messages.warning(request, "Your plan has expired. Please renew your subscription to continue.")
+                    # This redirect was already correct
                     return redirect('users:subscription_renew')
             except Subscription.DoesNotExist:
                 # This shouldn't happen because of your signal, but it's safe to handle
                 Subscription.objects.create(user=request.user)
                 messages.info(request, "Welcome! Your free plan has been set up.")
-
 
         # Continue to the requested view
         response = self.get_response(request)
